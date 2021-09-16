68cd46e912ddcb442d8d515485fa40838d16c6d9
byungmin-kang <byungmin.kang@snowcorp.com>
2021-09-16 22:27:14 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 강병민 on 2021/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/VocaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocaApp.swift
+//  Shared
+//
+//  Created by 강병민 on 2021/09/16.
+//
+
+import SwiftUI
+
+@main
+struct VocaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
