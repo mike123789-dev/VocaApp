6590f92cd429312eea25e50294d562bddfa17d5a
강병민 <byungmin.kang@snowcorp.com>
2022-01-01 19:30:06 +0900

    #1 단어 퀴즈 뷰 구현

--- Shared/Modules/VocaQuiz/VocaQuiz/VocaQuizView.swift
+++ Shared/Modules/VocaQuiz/VocaQuiz/VocaQuizView.swift
@@ -11,30 +11,31 @@ import ComposableArchitecture
 struct VocaQuizView: View {
     let store: Store<VocaQuizState, VocaQuizAction>
     
-    @State var current = 0.0
+
     var body: some View {
-        WithViewStore(store) { viewStore in
-            VStack {
-                QuizProgressHeader(currentCount: viewStore.currentIndex + 1,
-                                   totalCount: viewStore.totalCount)
-                Spacer()
-                ZStack {
-                    ForEach(viewStore.vocas.prefix(3)) { voca in
-                        SwipableCardView(
-                            content: {
-                                VocaCardView(store: .init(initialState: voca, reducer: vocaReducer, environment: .init(mainQueue: .main)))
-                            },
-                            willSwipeRight: viewStore.binding(\.$willSwipeRight),
-                            willSwipeLeft: viewStore.binding(\.$willSwipeLeft),
-                            didSwipeRight: nil,
-                            didSwipeLeft: nil
-                        )
+        GeometryReader { geometry in
+            WithViewStore(store) { viewStore in
+                VStack {
+                    QuizProgressHeader(currentCount: viewStore.currentCount,
+                                       totalCount: viewStore.totalCount)
+                    Spacer()
+                    CardStackView(viewStore: viewStore, geometry: geometry)
+                    Spacer()
+                    QuizFooter(viewStore: viewStore)
+                }
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        HStack {
+                            Text(viewStore.title)
+                                .font(.headline)
+                                .foregroundColor(.orange)
+                        }
                     }
                 }
-                Spacer()
-                QuizFooter(viewStore: viewStore)
             }
         }
+//        .edgesIgnoringSafeArea(.all)
     }
     
     struct QuizProgressHeader: View {
@@ -42,10 +43,46 @@ struct VocaQuizView: View {
         let totalCount: Int
         var body: some View {
             VStack {
-                Text("\(currentCount)/\(totalCount)")
+                Text("\(currentCount) / \(totalCount)")
                 ProgressView(value: Double(currentCount), total: Double(totalCount))
                     .frame(width: 150)
+                    .animation(.interactiveSpring(), value: currentCount)
             }
+        }
+    }
+    
+    struct CardStackView: View {
+        @ObservedObject var viewStore: ViewStore<VocaQuizState, VocaQuizAction>
+        let geometry: GeometryProxy
+        
+        private func getCardWidth(geometry: GeometryProxy, index: Int) -> CGFloat {
+            return geometry.size.width * 0.75 + CGFloat(index * 10)
+        }
+
+        private func getCardOffset(index: Int) -> CGFloat {
+            return -CGFloat(index * 10)
+        }
+
+        var body: some View {
+            ZStack {
+                ForEach(Array(viewStore.visableVocas.enumerated()), id: \.element.self) { (index, voca) in
+                    SwipableCardView(
+                        content: {
+                            VocaCardView(store: .init(initialState: voca, reducer: vocaReducer, environment: .init(mainQueue: .main)))
+                                .frame(width: self.getCardWidth(geometry: geometry, index: index), height: geometry.size.height * 0.6)
+                        },
+                        willSwipeRight: viewStore.binding(\.$willSwipeRight),
+                        willSwipeLeft: viewStore.binding(\.$willSwipeLeft),
+                        didSwipeRight: { viewStore.send(.swipe(voca, direction: .right),
+                                                        animation: .spring()) },
+                        didSwipeLeft: { viewStore.send(.swipe(voca, direction: .left),
+                                                       animation: .spring()) }
+                    )
+                    .offset(x: 0, y: self.getCardOffset(index: index))
+                    .transition(.scale.combined(with: .opacity))
+                }
+            }
+
         }
     }
     
@@ -53,13 +90,13 @@ struct VocaQuizView: View {
         @ObservedObject var viewStore: ViewStore<VocaQuizState, VocaQuizAction>
         var body: some View {
             HStack {
-                ScoreView(title: "\(viewStore.wrongCount)", color: .red, isFocused: viewStore.willSwipeLeft)
+                ScoreView(title: "\(viewStore.wrongCount)", position: .left, isFocused: viewStore.willSwipeLeft)
                 Spacer()
-                ScoreView(title: "\(viewStore.rightCount)", color: .green, isFocused: viewStore.willSwipeRight)
+                ScoreView(title: "\(viewStore.rightCount)", position: .right, isFocused: viewStore.willSwipeRight)
             }
             .overlay(alignment: .center) {
                 Button("재시작") {
-                    viewStore.send(.didTapResetButton)
+                    viewStore.send(.didTapResetButton, animation: .spring())
                 }
             }
             .padding(.vertical)
@@ -67,14 +104,37 @@ struct VocaQuizView: View {
     }
     
     struct ScoreView: View {
+        enum Position {
+            case left, right
+            
+            var color: Color {
+                switch self {
+                case .left:
+                    return .red
+                case .right:
+                    return .green
+                }
+            }
+            
+            var corners: UIRectCorner {
+                switch self {
+                case .left:
+                    return [.topRight, .bottomRight]
+                case .right:
+                    return [.topLeft, .bottomLeft]
+                }
+            }
+        }
+        
         let title: String
-        let color: Color
+        let position: Position
         let isFocused: Bool
         var body: some View {
             ZStack {
                 Rectangle()
-                    .fill(color)
-                    .frame(width: isFocused ? 70 : 50, height: 30, alignment: .leading)
+                    .fill(position.color)
+                    .frame(width: isFocused ? 60 : 50, height: 30, alignment: .leading)
+                    .cornerRadius(2, corners: position.corners)
                 Text(title)
                     .foregroundColor(.white)
             }
@@ -85,6 +145,29 @@ struct VocaQuizView: View {
 
 struct QuizView_Previews: PreviewProvider {
     static var previews: some View {
-        VocaQuizView(store: .init(initialState: .init(group: .test1), reducer: vocaQuizReducer, environment: .init()))
+        NavigationView {
+            Group {
+                VocaQuizView(store: .init(initialState: .init(group: .quizTest), reducer: vocaQuizReducer, environment: .init()))
+                VocaQuizView(store: .init(initialState: .init(group: .quizTest), reducer: vocaQuizReducer, environment: .init()))
+                    .previewDevice("iPhone SE (2nd generation)")
+            }
+        }
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
