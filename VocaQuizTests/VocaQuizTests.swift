fb984fcfe6df656d934bfc99aff51cb01c11b5be
강병민 <byungmin.kang@snowcorp.com>
2022-01-08 13:57:32 +0900

    VocaQuiz 테스트 추가

--- VocaQuizTests/VocaQuizTests.swift
+++ VocaQuizTests/VocaQuizTests.swift
@@ -4,32 +4,102 @@
 //
 //  Created by USER on 2022/01/08.
 //
-
+import ComposableArchitecture
 import XCTest
+@testable import Voca
 
 class VocaQuizTests: XCTestCase {
+    let scheduler = DispatchQueue.test
+    let store = TestStore(
+        initialState: VocaQuizState(group: .testGroup),
+        reducer: vocaQuizReducer,
+        environment: .init()
+    )
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testVocaQuizAllWrong() {
+        self.store.send(.swipe(.voca1, direction: .left)) {
+            $0.remainingVocas = [.voca2, .voca3]
+            $0.wrongVocas = [.voca1]
+        }
+        self.store.send(.swipe(.voca2, direction: .left)) {
+            $0.remainingVocas = [.voca3]
+            $0.wrongVocas = [.voca1, .voca2]
+        }
+        self.store.send(.swipe(.voca3, direction: .left)) {
+            $0.remainingVocas = []
+            $0.wrongVocas = [.voca1, .voca2, .voca3]
+        }
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testVocaQuizAllRight() {
+        self.store.send(.swipe(.voca1, direction: .right)) {
+            $0.remainingVocas = [.voca2, .voca3]
+            $0.rightVocas = [.voca1]
+        }
+        self.store.send(.swipe(.voca2, direction: .right)) {
+            $0.remainingVocas = [.voca3]
+            $0.rightVocas = [.voca1, .voca2]
+        }
+        self.store.send(.swipe(.voca3, direction: .right)) {
+            $0.remainingVocas = []
+            $0.rightVocas = [.voca1, .voca2, .voca3]
+        }
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func testVocaQuizReset() {
+        self.store.send(.swipe(.voca1, direction: .right)) {
+            $0.remainingVocas = [.voca2, .voca3]
+            $0.rightVocas = [.voca1]
+        }
+        self.store.send(.swipe(.voca2, direction: .right)) {
+            $0.remainingVocas = [.voca3]
+            $0.rightVocas = [.voca1, .voca2]
+        }
+        self.store.send(.swipe(.voca3, direction: .right)) {
+            $0.remainingVocas = []
+            $0.rightVocas = [.voca1, .voca2, .voca3]
+        }
+        self.store.send(.didTapResetButton) {
+            $0 = VocaQuizState(group: .testGroup)
         }
     }
 
+}
+
+extension Voca {
+    static let voca1 = Voca(
+        id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+        word: "voca1",
+        meaning: "voca1"
+    )
+    static let voca2 = Voca(
+        id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+        word: "voca2",
+        meaning: "voca2"
+    )
+    static let voca3 = Voca(
+        id: UUID(uuidString: "00000000-0000-0000-0000-000000000002")!,
+        word: "voca3",
+        meaning: "voca3"
+    )
+}
+
+extension VocaGroup {
+    static let testGroup = VocaGroup(
+        id: UUID(uuidString: "00000000-0000-0000-0000-000000000002")!,
+        title: "test",
+        items: .init(uniqueElements: [.voca1, .voca2, .voca3]),
+        isFavorite: false
+    )
+}
+
+extension UUID {
+  // A deterministic, auto-incrementing "UUID" generator for testing.
+  static var incrementing: () -> UUID {
+    var uuid = 0
+    return {
+      defer { uuid += 1 }
+      return UUID(uuidString: "00000000-0000-0000-0000-\(String(format: "%012x", uuid))")!
+    }
+  }
 }
